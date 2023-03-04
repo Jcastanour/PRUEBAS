7344a241bcf660ced2fb3fb0987c061940626341
jcastanour <jcastanour@unal.edu.co>
2023-03-04 15:01:17 -0500

    Agregue la carpeta pitagoras

---
+++ pitagoras/src/pitagoras/Pitagoras.java
@@ -0,0 +1,23 @@
+
+package pitagoras;
+
+//@author Juan Pablo
+
+import java.util.Scanner;
+
+public class Pitagoras {
+
+    public static void main(String[] args) {
+       double ladoa,ladob,hipo;
+       
+       Scanner entrada = new Scanner(System.in);
+       System.out.println("ingrese el valor del lado a: ");
+       ladoa = entrada.nextDouble();
+       System.out.println("ingrese el valor del lado b: ");
+       ladob = entrada.nextDouble();
+       
+       hipo = hipotenusa.calcularhipo(ladoa, ladob);
+       System.out.println("la hipotenusa es: " + hipo);
+    }
+
+}
---
+++ pitagoras/src/pitagoras/hipotenusa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package pitagoras;
+
+ // @author Juan Pablo
+public class hipotenusa {
+    public static double calcularhipo (double a,double b) {
+        
+        return Math.sqrt(Math.pow(a,2 )+Math.pow(b, 2));
+                
+    }
+    
+}
